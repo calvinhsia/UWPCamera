60361aa0e725cc9b18aaa9b5fad0a0e52f9d5605
CalvinH <calvinh@microsoft.com>
2017-03-27 16:47:44 -0700

    Add project files.

---
+++ UWPCamera/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPCamera
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var relPanel = new RelativePanel();
+                var spCtrls = new StackPanel()
+                {
+                    Orientation = Orientation.Horizontal
+                };
+                relPanel.Children.Add(spCtrls);
+                var sb = new StringBuilder();
+                for (int i = 0; i < 100; i++)
+                {
+                    sb.AppendLine($"xxx{i}");
+                }
+                //var devices = await DeviceInformation.FindAllAsync(DeviceClass.All);
+                //foreach (var device in devices)
+                //{
+                //    sb.AppendLine($"{device.Name} ID={device.Id} {device.ToString()}");
+                //    if (device.Properties != null)
+                //    {
+                //        foreach (var prop in device.Properties)
+                //        {
+                //            sb.AppendLine($"  K={prop.Key}  V={prop.Value?.ToString()}");
+                //        }
+                //    }
+                //}
+                sb.AppendLine("Done");
+                var txt = sb.ToString();
+                var vwr = new ScrollViewer();
+                RelativePanel.SetBelow(vwr, spCtrls);
+                var tb = new TextBlock()
+                {
+                    //Height = 200,
+                    Width = this.ActualWidth,
+                    //VerticalAlignment = VerticalAlignment.Stretch,
+                    TextWrapping = TextWrapping.Wrap,
+                };
+                vwr.Content = tb;
+                relPanel.Children.Add(vwr);
+                this.Content = relPanel;
+                tb.Text = txt;
+            }
+            catch (Exception ex)
+            {
+                this.Content = new TextBlock() { Text = ex.ToString() };
+            }
+        }
+    }
+}
+
+
